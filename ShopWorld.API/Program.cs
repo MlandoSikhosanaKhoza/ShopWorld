757f1385d74789ddf59ebd8e225e1068a4dafd58
mlando2.0 <mlando2.0@outlook.com>
2023-04-09 17:23:22 +0200

    Dependancy Injections Added

--- ShopWorld.API/Program.cs
+++ ShopWorld.API/Program.cs
@@ -1,3 +1,6 @@
+using ShopWorld.BusinessLogic;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#region Required Dependancy Injection
+DomainInjection.InjectBusinessLogic(builder.Services);
+DomainInjection.InjectJwtTokens(builder.Services, builder.Configuration["JWT:ValidAudience"], builder.Configuration["JWT:ValidIssuer"], builder.Configuration["JWT:Secret"]);
+DomainInjection.InjectCors(builder.Services);
+//Set you serialization options
+builder.Services.AddControllers(options =>
+    {
+        options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+    }).AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
+#endregion Required Dependancy Injection
 
 var app = builder.Build();
 
--- ShopWorld.BusinessLogic/DomainInjection.cs
+++ ShopWorld.BusinessLogic/DomainInjection.cs
@@ -1,7 +1,60 @@
-﻿namespace ShopWorld.BusinessLogic
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using ShopWorld.DataAccessLayer;
+using System.Text;
+
+namespace ShopWorld.BusinessLogic
 {
     public class DomainInjection
     {
+        public static void InjectBusinessLogic(IServiceCollection Services)
+        {
+            //Services.AddScoped<IAuthorizationLogic, AuthorizationLogic>();
 
+            Services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+
+        public static void InjectCors(IServiceCollection Services)
+        {
+            Services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder => builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader());
+            });
+        }
+
+        public static void InjectJwtTokens(IServiceCollection Services,string ValidAudience,string ValidIssuer,string Secret)
+        {
+            Services.AddAuthentication(options => {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidAudience = ValidAudience,
+                    ValidIssuer = ValidIssuer,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Secret))
+                };
+            });
+        }
+
+        //public static void InjectJsonOptions(IServiceCollection Services)
+        //{
+        //    Services.AddControllers(options =>
+        //    {
+        //        options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true;
+        //    }).AddJsonOptions(options =>
+        //    {
+        //        options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        //        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        //    });
+        //}
     }
 }
