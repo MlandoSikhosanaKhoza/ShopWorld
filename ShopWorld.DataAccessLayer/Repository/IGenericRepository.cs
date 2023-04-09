f4a22153fd52d28993d854fa9e71d5d8b9028372
mlando2.0 <mlando2.0@outlook.com>
2023-04-09 16:58:02 +0200

    Generic Repository and Unit of Work added

---
+++ ShopWorld.DataAccessLayer/Repository/IGenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopWorld.DataAccessLayer
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(object id);
+        Task<TEntity> GetByIdAsync(object id);
+        IEnumerable<TEntity> Get(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+        Task<IEnumerable<TEntity>> GetAsync(
+            Expression<Func<TEntity, bool>> filter = null,
+            Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>> orderBy = null,
+            string includeProperties = "");
+        TEntity Insert(TEntity entity);
+        bool Update(TEntity entity);
+        bool Delete(TEntity entity);
+    }
+}
