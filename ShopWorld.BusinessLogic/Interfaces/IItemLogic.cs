4108ea8c413e4be9973d17c2fc19153c0a722144
mlando2.0 <mlando2.0@outlook.com>
2023-04-17 18:50:06 +0200

    Really important for item logic

--- ShopWorld.BusinessLogic/Interfaces/IItemLogic.cs
+++ ShopWorld.BusinessLogic/Interfaces/IItemLogic.cs
@@ -1,4 +1,5 @@
-﻿using ShopWorld.Shared.Entities;
+﻿using ShopWorld.Shared;
+using ShopWorld.Shared.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +9,9 @@ namespace ShopWorld.BusinessLogic
     public interface IItemLogic
     {
         List<Item> GetAllItems();
-        Item AddItem(Item Item);
+        Item AddItem(ItemInputModel Item);
         Item GetItem(int ItemId);
-        bool UpdateItem(Item Item);
+        bool UpdateItem(ItemInputModel Item);
         bool DeleteItem(int ItemId);
     }
 }
