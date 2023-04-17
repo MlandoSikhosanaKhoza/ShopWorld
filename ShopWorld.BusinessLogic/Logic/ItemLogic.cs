b37a8126487f055a7a4d291764192429f1392c6b
mlando2.0 <mlando2.0@outlook.com>
2023-04-17 18:51:39 +0200

    Add and update image

--- ShopWorld.BusinessLogic/Logic/ItemLogic.cs
+++ ShopWorld.BusinessLogic/Logic/ItemLogic.cs
@@ -5,16 +5,20 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using Microsoft.AspNetCore.Hosting;
+
 namespace ShopWorld.BusinessLogic
 {
     public class ItemLogic:IItemLogic
     {
         private GenericRepository<Item> ItemRepository { get; set; }
         private IUnitOfWork _unitOfWork;
-        public ItemLogic(IUnitOfWork UnitOfWork)
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ItemLogic(IUnitOfWork UnitOfWork,IWebHostEnvironment webHostEnvironment)
         {
             _unitOfWork = UnitOfWork;
             ItemRepository = UnitOfWork.GetRepository<Item>();
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public List<Item> GetAllItems()
@@ -22,9 +26,11 @@ namespace ShopWorld.BusinessLogic
             return ItemRepository.Get(e => !e.IsDeleted).ToList();
         }
 
-        public Item AddItem(Item Item)
+
+        public Item AddItem(ItemInputModel ItemToAdd)
         {
-            Item itemAdded=ItemRepository.Insert(Item);
+            string path = StoreByteArrayFromBase64.Execute(ItemToAdd.Base64, $"{_webHostEnvironment.WebRootPath}/Images/", Path.GetExtension(ItemToAdd.ImageName));
+            Item itemAdded = ItemRepository.Insert(new Item { ImageName=Path.GetFileName(path), Description=ItemToAdd.Description, Price=ItemToAdd.Price,IsDeleted=false});
             _unitOfWork.SaveChanges();
             return itemAdded;
         }
@@ -34,18 +40,33 @@ namespace ShopWorld.BusinessLogic
             return ItemRepository.GetById(ItemId);
         }
 
-        public bool UpdateItem(Item Item)
+        public bool UpdateItem(ItemInputModel ItemToUpdate)
         {
-            ItemRepository.Update(Item);
+            Item item = GetItem(ItemToUpdate.ItemId);
+            string path,imageName;
+            
+            item.Description= ItemToUpdate.Description;
+            item.Price = ItemToUpdate.Price;
+            
+            if (!string.IsNullOrEmpty(ItemToUpdate.Base64))
+            {
+                DeleteExistingFile.Execute($"{_webHostEnvironment.WebRootPath}/Images/{item.ImageName}");
+                path = StoreByteArrayFromBase64.Execute(ItemToUpdate.Base64, $"{_webHostEnvironment.WebRootPath}/Images/{item.ImageName}", Path.GetExtension(ItemToUpdate.Base64));
+                imageName=Path.GetFileName(path);
+                item.ImageName = imageName;
+            }
+
+            ItemRepository.Update(item);
+            
             _unitOfWork.SaveChanges();
             return true;
         }
 
         public bool DeleteItem(int ItemId)
         {
-            Item item = GetItem(ItemId);
+            Item item = ItemRepository.GetById(ItemId);
             item.IsDeleted = true;
-            UpdateItem(item);
+            ItemRepository.Update(item);
             _unitOfWork.SaveChanges();
             return true;
         }
