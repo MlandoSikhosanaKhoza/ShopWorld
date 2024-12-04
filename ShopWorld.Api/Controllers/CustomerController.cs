d953b1398a7abc5e98e0324e62e042a458b9a1bd
Mlando Sikhosana Khoza <mlando2.0@outlook.com>
2024-12-04 21:49:42 +0200

    Add Controllers

---
+++ ShopWorld.Api/Controllers/CustomerController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShopWorld.BusinessLogic;
+using ShopWorld.Shared;
+using ShopWorld.Shared.Models;
+
+namespace ShopWorld.API.Controllers
+{
+    [Route("api/[controller][action]")]
+    [ApiController]
+    [Authorize(AuthenticationSchemes =JwtBearerDefaults.AuthenticationScheme)]
+    public class CustomerController : ControllerBase
+    {
+        /// <summary>
+        /// Please note that through out the project I've used the authorize attribute on the actual method. Here I'm white listing but throughout the project I blacklisted my authorize my endpoints.
+        /// </summary>
+        private readonly ICustomerLogic _customerLogic;
+        public CustomerController(ICustomerLogic customerLogic) {
+            _customerLogic = customerLogic;
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        [Produces("application/json", Type = typeof(CustomerModel))]
+        public IActionResult _GetCustomerById(int CustomerId)
+        {
+            return Ok(_customerLogic.GetCustomer(CustomerId));
+        }
+        [Authorize(Roles ="Admin")]
+        [HttpGet]
+        [Produces("application/json",Type=typeof(List<CustomerModel>))]
+        public IActionResult _GetCustomerList()
+        {
+            return Ok(_customerLogic.GetAllCustomers());
+        }
+        [Authorize(Roles ="Admin")]
+        [HttpGet]
+        [Produces("application/json", Type = typeof(List<CustomerModel>))]
+        public IActionResult _SearchCustomer(string Search)
+        {
+            return Ok(_customerLogic.SearchForCustomers(Search));
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        [Produces("application/json", Type = typeof(bool))]
+        public IActionResult _MobileNumberExists(string Mobile)
+        {
+            return Ok(_customerLogic.MobileNumberExists(Mobile));
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [Produces("application/json",Type = typeof(CustomerModel))]
+        public IActionResult _AddCustomer(CustomerModel CustomerObj) {
+            if (ModelState.IsValid)
+            {
+                return Ok(_customerLogic.AddCustomer(CustomerObj));
+            }
+            return BadRequest(ModelState.Values);
+        }
+        [AllowAnonymous]
+        [HttpGet]
+        [Produces("application/json", Type = typeof(CustomerModel))]
+        public IActionResult _GetCustomerByMobileNumber(string MobileNumber)
+        {
+            return Ok(_customerLogic.GetCustomerByMobileNumber(MobileNumber));
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        [Produces("application/json", Type = typeof(CustomerModel))]
+        public IActionResult _ConfigureCustomer(CustomerModel CustomerObj)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(_customerLogic.ConfigureCustomer(CustomerObj));
+            }
+            return BadRequest(ModelState.Values);
+        }
+    }
+}
